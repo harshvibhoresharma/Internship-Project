d5f31e53513cb5aadd22c2d77b83a6fd02c84860
Harsh Sharma <harsh.vibhore@gmail.com>
2025-06-18 12:35:08 +0530

    'routes'

---
+++ routes/userRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+
+router
+.post('/',async (req,res) => {
+    try{
+        const newUser = new User(req.body);
+        const savedUser=await newUser.save();
+        res.status(201).json(savedUser);
+
+    }catch(err){
+        res.status(400).json({error : err });
+    }
+
+});
+module.exports=router;
